3c42660bed6785e6184fb79b5f38273be3e3b39f
Slav1k <maznevslava@yandex.ru>
2022-08-22 16:32:30 +0500

    dz

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,40 @@
 public class Main {
-    public static void yaer(int year){
+    public static void yaer(int year) {
         System.out.print(year + "-");
     }
-    public static void yaerHot(){
-        System.out.print("високосный год");
+    public static int getClientOS(String name) {
+        if (name.equals("iOS")) {
+            return 0;
+        } else {
+            return 1;
         }
+    }
+
+    public static void yaerHot() {
+        System.out.print("високосный год");
+    }
+
     public static void yaerNot() {
         System.out.print("не високосный год");
     }
-        public static void main(String[] args) {
-            int year = 2021;
-            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                 yaer(year);
-                 yaerHot();
-            } else {
-                yaer(year);
-                yaerNot();
-                }
+
+    public static void main(String[] args) {
+        System.out.println("Задание №1:");
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            yaer(year);
+            yaerHot();
+        } else {
+            yaer(year);
+            yaerNot();
+        }
+        System.out.println();
+        System.out.println("Задание №2:");
+        int clientOS = 1; // 1=Andorid ; 0= iOS
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
